aee39d817157ec9d7b9994ff77c7086ac4fba18c
Loi <loichengllc@gmail.com>
2017-05-01 00:48:13 +0000

    ok

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 "user strict";
 
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = process.env.MONGOLAB_URI; 
+console.log(url);
 
 // set port
 var port = process.env.PORT || 8080;
@@ -15,3 +23,18 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
   console.log('Example app listening on port '+port);
 })
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established');
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
