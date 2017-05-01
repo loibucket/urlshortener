375670e084f3f3b33feaa6d8310d297869abe9a1
Loi <loichengllc@gmail.com>
2017-05-01 00:10:15 +0000

    first

---
+++ server.js
@@ -0,0 +1,17 @@
+"user strict";
+
+var express = require('express');
+var app = express();
+
+// set port
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+port);
+})
